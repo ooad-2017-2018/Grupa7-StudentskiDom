dc06457c0e0a7593b4fc812ee7a35477a6491f89
Dajana Mojsilović <dajana996@msn.com>
2018-05-09 23:00:52 +0200

    azury

--- Projekat/ProjekatStudentskiDom/ProjekatStudentskiDom/App.xaml.cs
+++ Projekat/ProjekatStudentskiDom/ProjekatStudentskiDom/App.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using ProjekatStudentskiDom.Klase;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace ProjekatStudentskiDom
 {
@@ -23,6 +24,7 @@ namespace ProjekatStudentskiDom
     /// </summary>
     sealed partial class App : Application
     {
+		public static MobileServiceClient MobileService = new MobileServiceClient("https://sabansaulic.azurewebsites.net");
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
